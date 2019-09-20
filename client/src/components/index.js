b867d67af815e86e80daa7fc3e52383f4098ece1
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-20 11:34:22 +0200

    Add absolute imports for components

---
+++ client/src/components/index.js
@@ -0,0 +1,33 @@
+import Logo from './common/Logo/Logo'
+import Pagination from './common/Pagination/Pagination';
+import Footer from './features/Footer/Footer';
+import NavBar from './features/NavBar/NavBar';
+import PreviewProduct from './features/PreviewProduct/PreviewProduct';
+import Products from './features/Products/Products';
+import Sidebar from './features/Sidebar/Sidebar';
+import MainLayout from './layouts/MainLayout/MainLayout';
+import MainMenu from './layouts/MainMenu/MainMenu';
+import PageContainer from './layouts/PageContainer/PageContainer';
+import Basket from './pages/Basket/Basket';
+import Contact from './pages/Contact/Contact';
+import FAQ from './pages/FAQ/FAQ';
+import Home from './pages/Home/Home';
+import Regulations from './pages/Regulations/Regulations';
+
+export {
+	Logo,
+    Pagination,
+    Footer,
+    NavBar,
+    PreviewProduct,
+    Products,
+    Sidebar,
+    MainLayout,
+    MainMenu,
+    PageContainer,
+    Basket,
+    Contact,
+    FAQ,
+    Home,
+    Regulations
+}
