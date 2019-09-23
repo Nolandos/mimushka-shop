f773b9ed8101527f513848b27a0ffb6fa7caa7d0
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-23 21:07:28 +0200

    Add PreviewProduct component

--- client/src/components/features/PreviewProduct/PreviewProduct.js
+++ client/src/components/features/PreviewProduct/PreviewProduct.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './PreviewProduct.scss';
 
-const PreviewProduct = () => (
+const PreviewProduct = (props) => (
   <div className="preview-product">
-    
+    <p className="preview-product__additional-info">{ props.additionalInfo }</p>
+    <div className="preview-product__photo-box">
+      <img className="preview-product__photo" alt="product-photo" src={ props.image }></img>
+    </div>
+    <p className="preview-product__name">{ props.name }</p>
+    <p className="preview-product__price">{ props.price }</p>
   </div>
 );
 
--- client/src/components/features/Products/Products.js
+++ client/src/components/features/Products/Products.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Products.scss';
 import { PreviewProduct } from '../../index';
-const elements = [1,2,3,4,5,6,7,8,9,10];
+import elements from './elements';
+
 const Products = () => (
   <div className="products">
-    {elements.map(item=><PreviewProduct key={item} />)}
+    {elements.map(item=><PreviewProduct {...item} key={item} />)}
   </div>
 );
 
