049aba8c213bb2fc28a2ef69f3fd0b41df73a10d
Łukasz Nolka <nolkalukasz@gmail.com>
2019-09-24 19:23:42 +0200

    Add pagination for products component

--- client/src/components/common/Pagination/Pagination.js
+++ client/src/components/common/Pagination/Pagination.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Pagination.scss';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-const Pagination = () => (
-  <div>
-    <h1>Pagination</h1>
-  </div>
-);
+const Pagination = (props) => {
+    const [presentPage, setPage] = useState(props.initialPage || 1);
+    const { pages, onPageChange } = props;
+
+    const changePage = (newPage) => {
+        setPage(newPage);
+        onPageChange(newPage);
+    }
+
+    const getNextPage = () => {
+        const nextPage = presentPage + 1;
+        setPage(nextPage);
+        onPageChange(nextPage);
+    }
+
+    const getPrevPage = () => {
+        const prevPage = presentPage - 1;
+        setPage(prevPage);
+        onPageChange(prevPage);
+    }
+
+    return (
+        <div className="pagination">
+            <ul className="pagination__list">
+                { presentPage > 1 && 
+                    <li 
+                        className="pagination__list__item" 
+                        onClick={() => getPrevPage()} >
+                        <IoIosArrowBack/>
+                    </li>
+                } 
+                {[...Array(pages)].map((el, page) =>
+                <li
+                    key={++page}
+                    onClick={() => { changePage(page) }}
+                    className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
+                    {page}
+                </li>
+                )}
+                {presentPage < pages && 
+                <li 
+                    className="pagination__list__item"
+                    onClick={() => getNextPage()} >
+                    <IoIosArrowForward/>
+                </li>}
+            </ul>
+        </div>
+    );
+};
+
+Pagination.propTypes = {
+    pages: PropTypes.number.isRequired,
+    initialPage: PropTypes.number,
+    onPageChange: PropTypes.func.isRequired,
+};
 
 export default Pagination;
--- client/src/components/features/Products/Products.js
+++ client/src/components/features/Products/Products.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './Products.scss';
-import { PreviewProduct } from '../../index';
+import { PreviewProduct, Pagination } from '../../index';
 import { loadProductsByPageRequest } from '../../../redux/productsReducer';
 
-const Products = (props) => {
+const Products = props => {
   const dispatch = useDispatch();
   const products = useSelector(({ products }) => products.data);
+  const pages = useSelector(({ products }) => Math.ceil(products.amount / products.productsPerPage));
 
   const [ initialPage ] = useState(props.initialPage || 1);
   const [ productsPerPage ] = useState(props.productsPerPage || 6);
@@ -17,9 +18,14 @@ const Products = (props) => {
     if( props.pagination === undefined) setPagination(true); 
   },[]);
 
+  const loadProductsPage = page => {
+    dispatch(loadProductsByPageRequest(page, productsPerPage));  
+  }
+
   return (
     <div className="products">
-      {products.map(item=><PreviewProduct {...item} key={item} />)}
+      {products.map(item=><PreviewProduct {...item} key={item.id} />)}
+      <Pagination pages={pages} initialPage={initialPage} onPageChange={(page) => loadProductsPage(page) } />
     </div>
   ) 
 };
